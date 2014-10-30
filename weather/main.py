4c88466b0ac512e13a4bc99b75e09dd505320fce
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-29 23:57:21 -0400

    factored out code from main into functions

--- weather/main.py
+++ weather/main.py
@@ -17,6 +17,19 @@ from forecastio import ForecastIO
 from grapher import (plot_temp_graph,
                      plot_pop_graph)
 
+# configuration options that can also be set on the command line
+config_keys = [
+    'apikey',
+    'location',
+    'output_dir',
+]
+
+# configuration options for which we cannot provide a default
+required_keys = [
+    'apikey',
+    'location',
+]
+
 def attr_setter(attr):
     def _(ele, val):
         ele.set(attr, val)
@@ -82,8 +95,9 @@ def parse_args():
     p = argparse.ArgumentParser()
     p.add_argument('--config', '-f',
                    default='weather.yaml')
-    p.add_argument('--output-dir', '-o',
-                   default='output')
+    p.add_argument('--output-dir', '-o')
+    p.add_argument('--apikey', '-k')
+    p.add_argument('--location', '-l')
 
     g = p.add_argument_group('Logging')
     g.add_argument('--debug', '-d',
@@ -98,6 +112,89 @@ def parse_args():
     p.set_defaults(loglevel=logging.WARN)
     return p.parse_args()
 
+def get_forecast_data():
+    logging.debug('getting forecast data')
+    f = ForecastIO(config['apikey'],
+                   cache_config=config.get('cache_config'))
+
+    data = f.forecast(config['location'])
+    return data
+
+
+def get_template():
+    logging.debug('parsing svg template')
+    svg_template = resource_stream(__name__,
+                                   'data/page1.svg')
+    template = etree.parse(svg_template)
+    svg_template.close()
+
+    return template
+
+
+def generate_temp_graph(data):
+    logging.info('generating temperature graph')
+    temp_data = [(h['time'], h['temperature'])
+                 for h in data['hourly']['data'][:12]]
+    plot_temp_graph(temp_data,
+                    output='{[output_dir]}/graph-temp.png'.format(
+                        config))
+
+
+def generate_pop_graph(data):
+    logging.info('generating precipitation graph')
+    pop_data = [(h['time'], h['precipProbability'])
+                for h in data['hourly']['data'][:12]]
+    plot_pop_graph(pop_data,
+                   output='{[output_dir]}/graph-pop.png'.format(
+                       config))
+
+
+def generate_graphs(data):
+    generate_temp_graph(data)
+    generate_pop_graph(data)
+
+
+def generate_svg(data):
+    template = get_template()
+
+    for t in ['temp', 'pop']:
+        sel = cssselect.CSSSelector('#graph-{}'.format(t))
+        ele = sel(template)
+        if not ele:
+            logging.error('selector %s failed to locate an element',
+                         sel)
+            raise ValueError(sel)
+        ele = ele[0]
+        ele.set('{{{[xlink]}}}href'.format(namespacemap),
+                'file://{}/{}'.format(
+                    os.path.abspath(config['output_dir']),
+                    'graph-{}.png'.format(t)
+                ))
+
+    for jp, spec in valuemap.items():
+        val = jsonpointer.resolve_pointer(data, jp)
+        val = spec['format'](val)
+        sel = cssselect.CSSSelector(spec['selector'])
+        ele = sel(template)
+        if not ele:
+            logging.error('failed to locate selector "%s" in template',
+                          spec['selector'])
+            sys.exit(1)
+        if len(ele) > 1:
+            logging.error('selector "%s" does not return a unique result',
+                          spec['selector'])
+            sys.exit(1)
+
+        ele = ele[0]
+
+        if 'setter' in spec:
+            spec['setter'](ele, val)
+        else:
+            ele.text = val
+
+    with open('{[output_dir]}/page1.svg'.format(config), 'w') as fd:
+        template.write(fd)
+
 def main():
     global config
 
@@ -108,69 +205,24 @@ def main():
         logging.info('reading config from %s',
                      args.config)
         config = yaml.load(fd)
+        config = config.get('weather', {})
 
-    config['weather'].setdefault('output_dir', args.output_dir)
+    for k in config_keys:
+        v = getattr(args, k)
+        if v is not None:
+            config[k] = v
 
-    svg_template = resource_stream(__name__,
-                                     'data/page1.svg')
-    doc = etree.parse(svg_template)
-    svg_template.close()
+    for k in required_keys:
+        if not k in config:
+            log.error('missing required configuration option %s',
+                      k)
+            sys.exit(2)
 
-    f = ForecastIO(config['weather']['apikey'],
-                   cache_config=config['weather'].get('cache_config'))
-    
-    data = f.forecast(config['weather']['location'])
-    temp_data = [(h['time'], h['temperature'])
-                 for h in data['hourly']['data'][:12]]
-    pop_data = [(h['time'], h['precipProbability'])
-                for h in data['hourly']['data'][:12]]
+    data = get_forecast_data()
+    generate_svg(data)
+    generate_graphs(data)
 
-    plot_temp_graph(temp_data,
-                    output='{[weather][output_dir]}/graph-temp.png'.format(
-                        config))
-    plot_pop_graph(pop_data,
-                    output='{[weather][output_dir]}/graph-pop.png'.format(
-                        config))
-
-    for t in ['temp', 'pop']:
-        sel = cssselect.CSSSelector('#graph-{}'.format(t))
-        ele = sel(doc)
-        if not ele:
-            logging.error('selector %s failed to locate an element',
-                         sel)
-            raise ValueError(sel)
-        ele = ele[0]
-        ele.set('{{{[xlink]}}}href'.format(namespacemap),
-                'file://{}/{}'.format(
-                    os.path.abspath(config['weather']['output_dir']),
-                    'graph-{}.png'.format(t)
-                ))
-
-    for jp,spec in valuemap.items():
-        val = jsonpointer.resolve_pointer(data, jp)
-        val = spec['format'](val)
-        sel = cssselect.CSSSelector(spec['selector'])
-        ele = sel(doc)
-        if not ele:
-            logging.error('failed to locate selector "%s" in template',
-                          spec['selector'])
-            raise KeyError(spec['selector'])
-        if len(ele) > 1:
-            logging.error('selector "%s" does not return a unique result',
-                          spec['selector'])
-            raise ValueError(spec['selector'])
-
-        ele = ele[0]
-
-        if 'setter' in spec:
-            spec['setter'](ele, val)
-        else:
-            ele.text = val
-
-    with open('%s/page1.svg' % config['weather']['output_dir'], 'w') as fd:
-        doc.write(fd)
-
-    os.chdir(config['weather']['output_dir'])
+    os.chdir(config['output_dir'])
     logging.info('generating output image')
     subprocess.check_call(['convert', 'page1.svg', 'page1.png'])
     logging.info('collapsing png colorspace')
