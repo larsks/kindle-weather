fcec9d6e652c8e4da9f7207140fca4b94fd8e6a9
Lars Kellogg-Stedman <lars@redhat.com>
2014-10-30 10:07:23 -0400

    refactored svg templating into svgtemplate module

--- weather/main.py
+++ weather/main.py
@@ -9,13 +9,15 @@ import jsonpointer
 import time
 from lxml import etree, cssselect
 import subprocess
-from pkg_resources import (Requirement,
-                           resource_filename,
-                           resource_stream)
 
 from forecastio import ForecastIO
+from svgtemplate import SVGTemplate
 from grapher import (plot_temp_graph,
                      plot_pop_graph)
+
+config = {
+    'output_dir': '.',
+}
 
 # configuration options that can also be set on the command line
 config_keys = [
@@ -29,12 +31,6 @@ required_keys = [
     'apikey',
     'location',
 ]
-
-def attr_setter(attr):
-    def _(ele, val):
-        ele.set(attr, val)
-
-    return _
 
 namespacemap = {
     'xlink': 'http://www.w3.org/1999/xlink',
@@ -63,7 +59,7 @@ valuemap = {
     '/daily/data/0/icon': {
         'selector': '#day_0_icon',
         'format': lambda x: '#{}'.format(iconmap.get(x, 'unknown')),
-        'setter': attr_setter('{{{[xlink]}}}href'.format(namespacemap)),
+        'attr': '{{{[xlink]}}}href'.format(namespacemap),
     },
 
     '/daily/data/1/temperatureMax': {
@@ -81,7 +77,7 @@ valuemap = {
     '/daily/data/1/icon': {
         'selector': '#day_1_icon',
         'format': lambda x: '#{}'.format(iconmap.get(x, 'unknown')),
-        'setter': attr_setter('{{{[xlink]}}}href'.format(namespacemap)),
+        'attr': '{{{[xlink]}}}href'.format(namespacemap),
     },
 
     '/currently/time': {
@@ -121,16 +117,6 @@ def get_forecast_data():
     return data
 
 
-def get_template():
-    logging.debug('parsing svg template')
-    svg_template = resource_stream(__name__,
-                                   'data/page1.svg')
-    template = etree.parse(svg_template)
-    svg_template.close()
-
-    return template
-
-
 def generate_temp_graph(data):
     logging.info('generating temperature graph')
     temp_data = [(h['time'], h['temperature'])
@@ -155,45 +141,24 @@ def generate_graphs(data):
 
 
 def generate_svg(data):
-    template = get_template()
+    template = SVGTemplate('data/page1.svg')
 
-    for t in ['temp', 'pop']:
-        sel = cssselect.CSSSelector('#graph-{}'.format(t))
-        ele = sel(template)
-        if not ele:
-            logging.error('selector %s failed to locate an element',
-                         sel)
-            raise ValueError(sel)
-        ele = ele[0]
-        ele.set('{{{[xlink]}}}href'.format(namespacemap),
-                'file://{}/{}'.format(
-                    os.path.abspath(config['output_dir']),
-                    'graph-{}.png'.format(t)
-                ))
-
-    for jp, spec in valuemap.items():
-        val = jsonpointer.resolve_pointer(data, jp)
-        val = spec['format'](val)
-        sel = cssselect.CSSSelector(spec['selector'])
-        ele = sel(template)
-        if not ele:
-            logging.error('failed to locate selector "%s" in template',
-                          spec['selector'])
-            sys.exit(1)
-        if len(ele) > 1:
-            logging.error('selector "%s" does not return a unique result',
-                          spec['selector'])
-            sys.exit(1)
-
-        ele = ele[0]
-
-        if 'setter' in spec:
-            spec['setter'](ele, val)
-        else:
-            ele.text = val
+    filemap = {
+        '#graph-temp': {
+            'attr': '{{{[xlink]}}}href'.format(namespacemap),
+            'value': 'file://{[output_dir]}/graph-temp.png'.format(config),
+        },
+        '#graph-pop': {
+            'attr': '{{{[xlink]}}}href'.format(namespacemap),
+            'value': 'file://{[output_dir]}/graph-pop.png'.format(config),
+        },
+    }
 
     with open('{[output_dir]}/page1.svg'.format(config), 'w') as fd:
-        template.write(fd)
+        fd.write(template.render(
+            data,
+            valuemap,
+            filemap))
 
 def main():
     global config
@@ -218,15 +183,11 @@ def main():
                       k)
             sys.exit(2)
 
+    config['output_dir'] = os.path.abspath(config['output_dir'])
+
     data = get_forecast_data()
     generate_svg(data)
     generate_graphs(data)
-
-    os.chdir(config['output_dir'])
-    logging.info('generating output image')
-    subprocess.check_call(['convert', 'page1.svg', 'page1.png'])
-    logging.info('collapsing png colorspace')
-    subprocess.check_call(['pngcrush', '-c', '0', '-ow', 'page1.png'])
 
 if __name__ == '__main__':
     f = main()
---
+++ weather/svgtemplate.py
@@ -0,0 +1,74 @@
+import jsonpointer
+from pkg_resources import (Requirement,
+                           resource_filename,
+                           resource_stream)
+from lxml import (etree,
+                  cssselect)
+import logging
+
+class SVGTemplate (object):
+    log = logging.getLogger(__name__)
+
+    def __init__(self, template_file):
+        self.template_file = template_file
+        self.load_template()
+
+    def load_template(self):
+        logging.debug('parsing svg template')
+        svg_template = resource_stream(__name__,
+                                       self.template_file)
+        self.template = etree.parse(svg_template)
+        svg_template.close()
+
+    def render(self, data, spec, constants):
+        '''Parameters:
+        
+        - data is an arbitrary dictionary
+        - spec is a mapping of css selectors to
+          jsonpointer selectors
+        - constants is a mapping of css selectors to
+          constant values
+        '''
+
+        self.update_constants(constants)
+        self.update_data(data, spec)
+
+        return etree.tostring(self.template)
+
+    def find_element(self, selector):
+        '''Locate an element in the template with a CSS selector.  Returns
+        the element, or None.'''
+
+        sel = cssselect.CSSSelector(selector)
+        ele = sel(self.template)
+        if not ele:
+            self.log.warn('selector %s failed to locate an element',
+                          selector)
+            return
+        if len(ele) > 1:
+            self.log.warn('selector %s resulted in multiple matches',
+                           selector)
+            return
+
+        return ele[0]
+
+    def update_data(self, data, spec):
+        for jp, spec in spec.items():
+            val = jsonpointer.resolve_pointer(data, jp)
+            val = spec['format'](val)
+
+            ele = self.find_element(spec['selector'])
+
+            if 'attr' in spec:
+                ele.set(spec['attr'], val)
+            else:
+                ele.text = val
+
+    def update_constants(self, constants):
+        for selector,spec in constants.items():
+            ele = self.find_element(selector)
+
+            if 'attr' in spec:
+                ele.set(spec['attr'], spec['value'])
+            else:
+                ele.text = spec['value']
